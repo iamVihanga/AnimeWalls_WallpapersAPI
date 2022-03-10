00d4c7d0aac7c55b5e2026ba10a80820d0d8a993
iamVihanga <vihangarashansilva@gmail.com>
2022-03-10 19:05:26 +0530

    Adding CORS Functionality

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from scraper import Scraper
 
 app = FastAPI()
 wallpapers_scraper = Scraper()
+
+# Handelling CORS
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def get_all():
